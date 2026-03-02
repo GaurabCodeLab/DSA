c291c5cc772e14f445db957644ccee74cb91d425
GaurabCodeLab <kumargaurab56@gmail.com>
2026-03-02 23:37:55 +0530

    code added

--- basic.py
+++ basic.py
@@ -94,4 +94,161 @@ def find_index(arr, x):
     return index
 
 result = find_index(arr, 10)
-print(result)
+# print(result)
+
+# Problem Statement:
+# Write a function secondLargest(arr) that returns the second largest distinct number in an array
+# arr = [0, 3, 5, 2, 7, 9]
+# arr = [4, 4, 4, 4, 4]
+# arr = [0, 3, 5, 5, 2, 7,7,7, 9, 9, 9]
+# arr = [5]
+arr =  [10, 20]
+def second_largest_number(arr):
+    if len(arr)<2:
+        return "Array should have at least two numbers"
+    first_largest = float("-inf")
+    second_largest = float("-inf")
+    for num in arr:
+        if num > first_largest:
+            second_largest = first_largest
+            first_largest = num
+        elif num > second_largest and num != first_largest:
+            second_largest = num
+    if second_largest == float("-inf"):
+        return "No second largest found"
+    return second_largest
+    
+
+# print(second_largest_number(arr))
+
+# Pattern 1: Print nxn Star Square
+# Print a square pattern of stars (*) of size n x n.
+# Output
+# * * * *
+# * * * *
+# * * * *
+# * * * *
+
+n = 4
+# for i in range(n):
+#     star = ""
+#     for j in range(n):
+#         star = star + "*"
+#     print(star)
+
+
+# Pattern 2: Right-Angled Star Triangle
+# Print a right-angled triangle of stars with n rows.
+# Output
+# *
+# * *
+# * * *
+# * * * *
+
+# for i in range(n):
+#     star = ""
+#     for j in range(i +1):
+#         star = star + "*"
+#     print(star)
+
+
+# Pattern 3: Print a Right-Angled Number Triangle
+# Write a program that prints a right-angled triangle of numbers of heightn.
+# Output
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+
+# for i in range(n):
+#     star = ""
+#     for j in range(i+1):
+#         star = star + str(j+1)
+#     print(star)
+
+
+# Pattern 4: Print a Right-Angled Triangle of Repeated Numbers
+# Write a program that prints a right-angled triangle where each row contains the same number repeated.
+# Output
+# 1
+# 2 2
+# 3 3 3
+# 4 4 4 4
+
+# for i in range(n):
+#     star = ""
+#     for j in range(i+1):
+#         star = star + str(i+1)
+#     print(star)
+
+
+# Pattern 5: Print a Reverse Right-Angled Triangle of Increasing Numbers
+# Write a program that prints a reverse right-angled triangle where each row starts from 1 and the number of elements decreases with each row.
+# Output
+# 1 2 3 4
+# 1 2 3
+# 1 2
+# 1
+
+# for i in range(n):
+#     star = ""
+#     for j in range(n-i):
+#         star = star + str(j+1)
+#     print(star)
+
+
+# Pattern 6: Print a Right-Aligned Right-Angled Triangle of Stars
+# Write a program that prints a right-aligned triangle of stars increasing row by row, with leading spaces for alignment.
+# Output
+#       *
+#     * *
+#   * * *
+# * * * *
+
+# for i in range(n):
+#     star = ""
+#     for j in range(n-i-1):
+#         star = star + " "
+#     for k in range(i+1):
+#         star = star + "*"
+#     print(star)
+
+
+# Pattern 7: Print a Right-Angled Triangle of Alternating 1s and 0s
+# Write a program that prints a triangle of alternating 1s and 0s starting with 1 on each row.
+# Output
+# 1
+# 1 0
+# 1 0 1
+# 1 0 1 0
+
+# for i in range(n):
+#     star = ""
+#     toggle = 1
+#     for k in range(i+1):
+#         star = star + str(toggle)
+#         if toggle == 1:
+#             toggle = 0
+#         else:
+#             toggle = 1
+#     print(star)
+
+
+# Pattern 8: Right-Angled Triangle of Alternating 1s and 0s (Global Toggle)
+# Write a program to print a triangle of alternating 1s and 0s, but the toggle continues globally across rows.
+# Output
+# 1
+# 0 1
+# 0 1 0
+# 1 0 1 0
+
+# toggle = 1
+# for i in range(n):
+#     star = ""
+#     for j in range(i +1):
+#         star = star + str(toggle)
+#         if toggle == 1:
+#             toggle = 0
+#         else:
+#             toggle = 1
+#     print(star)
