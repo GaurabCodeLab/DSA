cb9c0d8a99ce283950b99fdd3fd62ba884b23e11
GaurabCodeLab <kumargaurab56@gmail.com>
2026-03-27 22:54:46 +0530

    more questiona added

--- array-problem.js
+++ array-problem.js
@@ -100,19 +100,102 @@
 
 // Problem Statement:
 // Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
-function required(arr) {
-  let x = 0;
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > arr[x]) {
-      x = x + 1;
-      arr[x] = arr[i];
-    }
-  }
-  for (let i = x + 1; i < arr.length; i++) {
-    arr[i] = "_";
-  }
-  console.log(arr);
-  return x + 1;
-}
+// function required(arr) {
+//   let x = 0;
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > arr[x]) {
+//       x = x + 1;
+//       arr[x] = arr[i];
+//     }
+//   }
+//   for (let i = x + 1; i < arr.length; i++) {
+//     arr[i] = "_";
+//   }
+//   console.log(arr);
+//   return x + 1;
+// }
 
-console.log(required([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+// console.log(required([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+
+// Problem Statement:
+// You are given an array prices where prices[i] is the price of a given stock on the ith day. You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+// First Method: Brute Force Approach
+// function required(arr) {
+//   let maxProfit = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[j] - arr[i] > maxProfit) {
+//         maxProfit = arr[j] - arr[i];
+//       }
+//     }
+//   }
+//   return maxProfit;
+// }
+
+// console.log(required([7, 6, 4, 3, 1]));
+
+// Second Method:-
+// function required(arr) {
+//   let maxProfit = 0;
+//   let buyPrice = arr[0];
+//   for (i = 1; i < arr.length; i++) {
+//     if (buyPrice > arr[i]) {
+//       buyPrice = arr[i];
+//     }
+//     if (arr[i] - buyPrice > maxProfit) {
+//       maxProfit = arr[i] - buyPrice;
+//     }
+//   }
+//   return maxProfit;
+// }
+
+// console.log(required([7, 6, 4, 3, 1]));
+
+// Problem Statement:
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+// Mergenums1 and nums2 into a single array sorted in non-decreasing order.
+// Firest Method: Brute Force Method:-
+// function required(arr1, m, arr2, n) {
+//   for (let i = m; i < arr1.length; i++) {
+//     arr1[i] = arr2[i - m];
+//   }
+//   arr1.sort((a, b) => a - b);
+//   return arr1;
+// }
+// console.log(required([0], 0, [1], 1));
+
+// Second Method:-
+// function required(arr1, m, arr2, n) {
+//   let arr1Copy = arr1.slice(0, m);
+//   let p1 = 0;
+//   let p2 = 0;
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (p2 >= n || (p1 < m && arr2[p2] > arr1Copy[p1])) {
+//       arr1[i] = arr1Copy[p1];
+//       p1++;
+//     } else {
+//       arr1[i] = arr2[p2];
+//       p2++;
+//     }
+//   }
+//   return arr1;
+// }
+// console.log(required([0], 0, [1], 1));
+
+// Third Method:-
+// function required(arr1, m, arr2, n) {
+//   let p1 = m - 1;
+//   let p2 = n - 1;
+//   for (let i = m + n - 1; i >= 0; i--) {
+//     if (p2 < 0) break;
+//     if (p1 >= 0 && arr1[p1] > arr2[p2]) {
+//       arr1[i] = arr1[p1];
+//       p1--;
+//     } else {
+//       arr1[i] = arr2[p2];
+//       p2--;
+//     }
+//   }
+//   return arr1;
+// }
+// console.log(required([0], 0, [1], 1));
