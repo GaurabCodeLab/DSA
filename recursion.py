6ad6cebc8177e3710b609790cd006a8fbba1af37
GaurabCodeLab <kumargaurab56@gmail.com>
2026-04-28 16:55:31 +0530

    recursion module added

---
+++ recursion.py
@@ -0,0 +1,32 @@
+# Problem: Print numbers from n to 1 using recursion.
+# n = 12
+# def func(num):
+#     if num==0:
+#         return
+#     print(num)
+#     num = num-1
+#     func(num)
+
+# func(n)
+# Time complexity = O(n)
+# Space complexity = O(n)
+
+# Problem: Print numbers from 1 to n using recursion.
+# n = 12
+# def func(num):
+#     if num==0:
+#         return
+#     print(n-num+1)
+#     num = num - 1
+#     func(num)
+
+# func(n)
+
+# Write a function sum(n) that calculates the sum of the first n natural numbers using recursion.
+# n = 12
+# def func(num):
+#     if num==1:
+#         return 1
+#     return num+func(num-1)
+
+# print(func(n))
